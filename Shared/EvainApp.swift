bf8bc54c55ab50f8cfadc6f69b52a64a3a658a5d
Jatin <jatinx.lol@gmail.com>
2020-07-02 09:50:40 +0900

    Initial Commit

---
+++ Shared/EvainApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvainApp.swift
+//  Shared
+//
+//  Created by Jatin on 2020/07/02.
+//
+
+import SwiftUI
+
+@main
+struct EvainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
